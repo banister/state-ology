a9700be7e61a2508575d53b818349ccd3e8497a7
John Mair <jrmair@gmail.com>
2008-12-05 04:37:07 +1300

    got rid of debugging info

--- lib/stateology.rb
+++ lib/stateology.rb
@@ -18,180 +18,180 @@ module Stateology
 
     # class methods
     module SM_Class_Methods
-        def state(name, &block)      
-            
+        def state(name, &block)
+
             # if const is defined here then module_eval
             if constants.include?(name.to_s) && const_defined?(name) then
                 self.module_eval(&block)
             else
-                
-                m = Module.new    
-                # if the state is defined further up the chain then "inherit it"        
+
+                m = Module.new
+                # if the state is defined further up the chain then "inherit it"
                 if constants.include?(name.to_s) && !const_defined?(name) then
                     # if constant not defined here then must be inherited
                     inherited_state = const_get(name)
-            
-                    # ignore if the constant is not a module                                
-                    m.send(:include, inherited_state) if inherited_state.instance_of?(Module)  
+
+                    # ignore if the constant is not a module
+                    m.send(:include, inherited_state) if inherited_state.instance_of?(Module)
                 end
-                
-                m.send(:include, Stateology)    
-                m.module_eval(&block)           
-                
-                const_set(name, m)                                                                                        
-            end            
+
+                m.send(:include, Stateology)
+                m.module_eval(&block)
+
+                const_set(name, m)
+            end
         end
     end
-    
+
     # strip the class path and return just the constant name, i.e Hello::Fren -> Fren
     def __elided_class_path(sym)
         "#{sym}".split(/::/).last.intern
     end
-    
+
     def __sym_to_mod(sym)
         class << self; self; end.const_get(sym)
     end
-    
+
     def __mod_to_sym(mod)
         # weird case where module does not have name (i.e when a state created on the eigenclass)
         if mod.name == "" then
-            class << self; self; end.constants.each do |v|                 
-                return v.to_sym if __sym_to_mod(v.to_sym) == mod                
+            class << self; self; end.constants.each do |v|
+                return v.to_sym if __sym_to_mod(v.to_sym) == mod
             end
             return :ConstantNotDefined
-        end 
-                
+        end
+
         mod.name.to_sym
     end
-    
+
     # is state_name a nested state?
     def __nested_state?(new_state)
-    
-        # test is: 
+
+        # test is:
         # (1) are we currently in a state? (non nil)
         # (2) is the new state a state? (non nil)
         # (3) is the new state defined under the current state? (i.e it's nested)
-        @__SM_nesting.first && 
-        new_state && 
+        @__SM_nesting.first &&
+        new_state &&
         @__SM_nesting.first.const_defined?(__elided_class_path(__mod_to_sym(new_state)))
     end
-        
-    
-    # instance methods   
+
+
+    # instance methods
     def __state_epilogue
-                        
-        @__SM_nesting.each do |old_state|    
+
+        @__SM_nesting.each do |old_state|
             raise NameError if !old_state.instance_of?(Module) && old_state != nil
-              
+
             begin
                 state_exit()
             rescue NoMethodError
                 # do nothing
             end
-           
+
             if old_state then unmix(old_state) end
         end
         @__SM_nesting = []
     end
-        
+
     def __state_prologue(new_state, state_args, &block)
-    
+
         # ensure that the constant is a module
         raise NameError if !new_state.instance_of?(Module) && new_state != nil
-        
+
         # only mixin if non-nil (duh)
-        if new_state then extend(new_state) end        
-        
-        begin  
-            state_entry(*state_args, &block)                
+        if new_state then extend(new_state) end
+
+        begin
+            state_entry(*state_args, &block)
         rescue NoMethodError
             # do nothing
         end
-        
+
     end
-    
-    def __validate_state_name(state_name)   
+
+    def __validate_state_name(state_name)
         # if we receive a Symbol convert it to a constant
         if Symbol === state_name then
             state_name = __sym_to_mod(state_name)
         end
-        
+
         raise NameError if state_name && !state_name.instance_of?(Module)
-        
+
         state_name
     end
-        
-        
-    
+
+
+
     def __state_transition(new_state, state_args, &block)
         # preven unnecessary state transition
         return if @__SM_nesting.first == new_state
-        
+
         # get rid of state_name from arg list
         state_args.shift
-        # exit old state only if the new state is not nested within it                     
-        __state_epilogue unless __nested_state?(new_state)                                                                                                             
-        __state_prologue(new_state, state_args, &block)   
-        
+        # exit old state only if the new state is not nested within it
+        __state_epilogue unless __nested_state?(new_state)
+        __state_prologue(new_state, state_args, &block)
+
         @__SM_nesting.unshift(new_state)
-    end 
-    
+    end
+
     def __state_getter
         @__SM_nesting.first ? __elided_class_path(__mod_to_sym(@__SM_nesting.first)) : nil
-    end   
-    
+    end
+
     def __current_state
         @__SM_nesting.first
-    end       
-    
+    end
+
     def __init_state
-        @__SM_nesting ||= [nil] 
-    end                   
-        
-        
+        @__SM_nesting ||= [nil]
+    end
+
+
     def state(*state_args, &block)
         __init_state
-           
+
         # behave as getter
-        if state_args.empty? then            
+        if state_args.empty? then
             return __state_getter
         end
-        
+
         new_state = __validate_state_name(state_args.first)
-                                     
+
         __state_transition(new_state, state_args, &block)
-                                    
+
         # return value is the current state
         __current_state
-        
-      #  rescue NameError
-      #      raise NameError, "#{new_state} not a valid state" 
-                                        
+
+        rescue NameError
+            raise NameError, "#{new_state} not a valid state"
+
     end
-    
+
     # is the current state equal to state_name?
     def state?(state_name)
         __init_state
-            
+
         state_name = __validate_state_name(state_name)
-          
+
         # compare
         state_name == __current_state
-        
+
         rescue NameError
-          raise NameError, "#{state_name} not a valid state" 
-                                                            
+          raise NameError, "#{state_name} not a valid state"
+
     end
-    
+
     # return the current state as a module
     def state_mod
         __init_state
         @__SM_nesting.first
     end
-                   
-    private :__state_prologue, :__state_epilogue, :__elided_class_path, :__mod_to_sym, :__sym_to_mod, 
+
+    private :__state_prologue, :__state_epilogue, :__elided_class_path, :__mod_to_sym, :__sym_to_mod,
         :__nested_state?, :__current_state
-        
+
 end
 
 
